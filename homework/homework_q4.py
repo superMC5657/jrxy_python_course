331b056eab11761c9960d2488f72f01465857760
zhangyan <18758266469@163.com>
2022-01-02 01:05:12 +0800

    Q4.4

--- homework/homework_q4.py
+++ homework/homework_q4.py
@@ -6,6 +6,8 @@
 
 #### Answer for Q4
 ## Q4.1
+import math
+
 import numpy as np
 import pandas as pd
 import tushare as ts
@@ -23,61 +25,104 @@ for ts_code in ts_code_list:
 
 close_list = []
 for daily in daily_list:
+    daily = daily.set_index(['trade_date'], inplace=False)
     close_list.append(daily['close'].rename(daily['ts_code'][0]))
 close_df = pd.concat(close_list, axis=1)
 print(close_df)
 
-
-
 ## Q4.2
-pct_change_list = []
-for daily in daily_list:
-    pct_change = daily['pct_chg']
-    pct_change.name = daily['ts_code'][0]
-    pct_change_list.append(pct_change)
-
-pct_change_df = pd.concat(pct_change_list, axis=1)
-
 import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
 
-f, ax = plt.subplots(figsize=(14, 10))
-corrdf = pct_change_df.corr()
-sns.heatmap(corrdf, cmap='RdBu', linewidths=0.05, ax=ax)
+# for daily in daily_list:
+#     close_pct_change = daily[['pct_chg', 'close']]
+#     f, ax = plt.subplots(figsize=(14, 10))
+#     corrdf = close_pct_change.corr()
+#     sns.heatmap(corrdf, cmap='RdBu', linewidths=0.05, ax=ax)
+#
+#     # 设置Axes的标题
+#     ax.set_title(daily['ts_code'][0])
+#     plt.show()
 
-# 设置Axes的标题
-ax.set_title('Correlation between stocks')
+
+# pct_change_list = []
+# for daily in daily_list:
+#     pct_change = daily['pct_chg']
+#     pct_change.name = daily['ts_code'][0]
+#     pct_change_list.append(pct_change)
+#
+# pct_change_df = pd.concat(pct_change_list, axis=1)
+#
+# f, ax = plt.subplots(figsize=(14, 10))
+# corrdf = pct_change_df.corr()
+# sns.heatmap(corrdf, cmap='RdBu', linewidths=0.05, ax=ax)
+#
+# # 设置Axes的标题
+# ax.set_title('Correlation between stocks')
+# plt.show()
+#
+#
+# ## Q4.3
+# def MaxDrawdown(returns):
+#     l = np.argmax((np.maximum.accumulate(returns) - returns) / np.maximum.accumulate(returns))
+#     k = np.argmax(returns[:l])
+#     return (returns[k] - returns[l]) / (returns[l])
+#
+#
+# def cal_half_def(returns):
+#     for i in range(len(returns)):
+#         returns[i] = returns[0] - returns[i]
+#     mu = returns.mean()  # 这里使用的是均值
+#     temp = returns[returns < mu]
+#     half_deviation = (sum((temp - mu) ** 2) / len(temp)) ** 0.5
+#     return half_deviation
+#
+#
+# stocks = []
+# for daily in daily_list:
+#     close_list = daily['close'].values.tolist()
+#     close_list.reverse()
+#     maximum_pullback = MaxDrawdown(close_list)
+#     half_deviation = cal_half_def(np.array(close_list))
+#     print(maximum_pullback, half_deviation)
+#     stocks.append((daily['ts_code'][0], maximum_pullback, half_deviation))
+# stocks.sort(key=lambda x: x[1])
+# stocks = stocks[:len(stocks) // 2]
+# stocks.sort(key=lambda x: x[2])
+# stocks = stocks[:len(stocks) // 2]
+# for stock in stocks:
+#     print(stock[0])
+
+## Q4.4
+from matplotlib import pyplot as plt
+
+fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(20, 20))
+close_df = close_df.dropna().iloc[::-1]
+close_df_sub = close_df - close_df.iloc[0, :]
+
+
+
+ax[0].plot(close_df_sub)
+ax[0].set_title('Close Price')
+close_df['mean'] = close_df.mean(axis=1)
+
+
+def get_ema_std(six_days):
+    ema = sum(six_days) / len(six_days)
+    std = sum((six_days - ema) ** 2) / len(six_days)
+    return ema, std
+
+
+ema_list = []
+
+for i in range(6, len(close_df['mean'])):
+    six_days = close_df['mean'][i - 6:i]
+    ema, std = get_ema_std(six_days)
+    ema_list.append(ema)
+
+close_df = close_df[6:]
+close_df['ema'] = ema_list
+close_df_mean_ema = close_df[['mean', 'ema']]
+close_df_mean_ema.plot(ax=ax[1])
 plt.show()
-
-
-## Q4.3
-def MaxDrawdown(returns):
-    l = np.argmax((np.maximum.accumulate(returns) - returns) / np.maximum.accumulate(returns))
-    k = np.argmax(returns[:l])
-    return (returns[k] - returns[l]) / (returns[l])
-
-
-def cal_half_def(returns):
-    for i in range(len(returns)):
-        returns[i] = returns[0] - returns[i]
-    mu = returns.mean()  # 这里使用的是均值
-    temp = returns[returns < mu]
-    half_deviation = (sum((temp - mu) ** 2) / len(temp)) ** 0.5
-    return half_deviation
-
-
-stocks = []
-for daily in daily_list:
-    close_list = daily['close'].values.tolist()
-    close_list.reverse()
-    maximum_pullback = MaxDrawdown(close_list)
-    half_deviation = cal_half_def(np.array(close_list))
-    print(maximum_pullback, half_deviation)
-    stocks.append((daily['ts_code'][0], maximum_pullback, half_deviation))
-stocks.sort(key=lambda x: x[1])
-stocks = stocks[:len(stocks) // 2]
-stocks.sort(key=lambda x: x[2])
-stocks = stocks[:len(stocks) // 2]
-for stock in stocks:
-    print(stock[0])
